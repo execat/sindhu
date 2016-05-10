136c64ebe3cc81a0ee24059de28e589863366f4f
Anuj More <anujmorex@gmail.com>
2016-05-10 14:03:37 -0500

    Solving 'similar vowels' sandhi rule

--- syllables.pl
+++ syllables.pl
@@ -2,9 +2,19 @@
 %% Vowels
 %%
 
+%% List of vowels
 vowel("a"). vowel("ā"). vowel("i"). vowel("ī"). vowel("u"). vowel("ū").
 vowel("ṛ"). vowel("ṝ"). vowel("ḷ"). vowel("ḹ"). vowel("e"). vowel("ai").
 vowel("o"). vowel("au").
+
+%% Establish relation between long and short vowels
+long("a", "ā").
+long("i", "ī").
+long("u", "ū").
+long("ṛ", "ṝ").
+long("ḷ", "ḹ").
+long("e", "ai").
+long("o", "au").
 
 %% Strength heirarchy of vowels
 strong("i", "e"). strong("ī", "e"). strong("e", "ai").
@@ -31,6 +41,8 @@ semivowel("y"). semivowel("r"). semivowel("l"). semivowel("v").
 s_sound("ś"). s_sound("ṣ"). s_sound("s").
 %% h-sounds
 h_sound("h").
+%% no sound
+no_sound("").
 
 %% terminal/2 :: A terminal letter from each varga
 terminal(kavarga, "ṅ"). terminal(cavarga, "ñ"). terminal(ṭavarga, "ṇ").
@@ -49,6 +61,7 @@ syllable(X):- pavarga(X).
 syllable(X):- semivowel(X).
 syllable(X):- s_sound(X).
 syllable(X):- h_sound(X).
+syllable(X):- no_sound(X).
 
 %% syllable/2 :: A syllable given its varga
 syllable(kavarga, X):- kavarga(X).
@@ -59,6 +72,7 @@ syllable(pavarga, X):- pavarga(X).
 syllable(semivowel, X):- semivowel(X).
 syllable(s_sound, X):- s_sound(X).
 syllable(h_sound, X):- h_sound(X).
+syllable(no_sound, X):- no_sound(X).
 
 %%
 %% Consonant forming rules
@@ -71,11 +85,51 @@ consonant(X, Ans):-
 
 %% compound_consonant/2 :: A compound consonant is a letter from same varga
 compound_consonant(X, Y, Ans):-
-    syllable(Varga, X), syllable(Varga, Y),
-    terminal(Varga, Y), X \== Y,
-    string_concat(Y, "a", Complete_Y),
-    string_concat(X, Complete_Y, Ans).
+    syllable(X), syllable(Y),
+    string_concat(X, Y, Ans).
 
 %%
 %% Sandhi rules
 %%
+
+% Similar vowels rule
+% Example:
+% sandhi(["bha", "va"], ["a", "r", "ju", "na"], Ans).
+% ["bha", "vā", "r", "ju", "na"]
+sandhi(Xs, Ys, Ans):-
+    % Split
+    split(Xs, Xs_first, Xs_rest, Xs_last),
+    split(Ys, Ys_first, Ys_rest, Ys_last),
+    % Test
+    % Condition: similar vowels while combining
+    Xs_last = [Xs_last_string],
+    Ys_first = [Ys_first_string],
+    decompose(Sx_syllable, Sx_vowel, Xs_last_string),
+    decompose(Sy_syllable, Sy_vowel, Ys_first_string),
+    Sx_vowel == Sy_vowel,
+    Sy_syllable == "",
+
+    % Combine
+    long(Sy_vowel, S_combine),
+    string_concat(Sx_syllable, S_combine, S_Ans),
+    S_combine_word = [S_Ans],
+
+    append(Xs_first, Xs_rest, Ans_1),
+    append(Ys_rest, Ys_last, Ans_3),
+    append(Ans_1, S_combine_word, Ans_2),
+    append(Ans_2, Ans_3, Ans).
+
+%% Decompose helper
+decompose(Syllable, Vowel, Ans):-
+    syllable(Syllable),
+    vowel(Vowel),
+    string_concat(Syllable, Vowel, Ans).
+
+%% Split helper
+split(Xs, Xs_first, Xs_rest, Xs_last):-
+    append(Xs_1, Xs_last, Xs),
+    length(Xs_last, 1),
+    append(Xs_first, Xs_rest, Xs_1).
+    % With this, you can solve "bhava + arjuna" to get solution once
+    % But not get solution for "su + ukta"
+    % length(Xs_first, 1).
